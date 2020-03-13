60bb65894e57cc77990483422a09a5cd2e967af9
Shaun <shaun@thenetninja.co.uk>
2020-03-13 16:40:34 +0000

    lesson-5

--- sandbox.js
+++ sandbox.js
@@ -1,28 +1,24 @@
+var character = 'mario';
+var age;
+var isLoggedIn;
+// age = 'luigi';
+age = 30;
+// isLoggedIn = 25;
+isLoggedIn = true;
 // arrays
-var names = ['luigi', 'mario', 'yoshi'];
-names.push('toad');
-// names.push(3);
-// names[1] = 3;
-var numbers = [10, 20, 12, 15];
-numbers.push(25);
-// numbers.push('shaun');
-// numbers[0] = 'shaun';
-var mixed = ['ken', 4, 'chun-li', 8, 9];
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
+var ninjas = [];
+ninjas.push('ryu');
+ninjas.push('chun-li');
+console.log(ninjas);
+// union types
+var mixed = [];
+mixed.push('hello');
+mixed.push(false);
+mixed.push(20);
+console.log(mixed);
+var uid;
 // objects
-var ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 30
-};
-ninja.age = 40;
-ninja.name = 'ryu';
-// ninja.age = '30';
-// ninja.skills = ['fighting', 'sneaking']
-ninja = {
-    name: 'yoshi',
-    belt: 'orange',
-    age: 40
-};
+var ninjaOne;
+ninjaOne = { name: 'yoshi', age: 30 };
+var ninjaTwo;
+ninjaTwo = { name: 'ken', age: 20, beltColour: 'black' };
--- sandbox.ts
+++ sandbox.ts
@@ -1,37 +1,36 @@
+let character: string = 'mario';
+let age: number;
+let isLoggedIn: boolean;
+
+// age = 'luigi';
+age = 30;
+
+// isLoggedIn = 25;
+isLoggedIn = true;
+
 // arrays
-let names = ['luigi', 'mario', 'yoshi'];
+let ninjas: string[] = [];
 
-names.push('toad');
-// names.push(3);
-// names[1] = 3;
+ninjas.push('ryu');
+ninjas.push('chun-li');
+console.log(ninjas);
 
-let numbers = [10, 20, 12, 15];
+// union types
+let mixed: (string|number|boolean)[] = [];
+mixed.push('hello');
+mixed.push(false);
+mixed.push(20);
+console.log(mixed);
 
-numbers.push(25);
-// numbers.push('shaun');
-// numbers[0] = 'shaun';
-
-let mixed = ['ken', 4, 'chun-li', 8, 9];
-
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
+let uid: string|number;
 
 // objects
-let ninja = {
-  name: 'mario',
-  belt: 'black',
-  age: 30
-};
+let ninjaOne: object;
+ninjaOne = { name: 'yoshi', age: 30 };
 
-ninja.age = 40;
-ninja.name = 'ryu';
-// ninja.age = '30';
-// ninja.skills = ['fighting', 'sneaking']
-
-ninja = {
-  name: 'yoshi',
-  belt: 'orange',
-  age: 40,
-  // skills: ['running'],
+let ninjaTwo: {
+  name: string,
+  age: number,
+  beltColour: string
 };
+ninjaTwo = { name: 'ken', age: 20, beltColour: 'black' };
