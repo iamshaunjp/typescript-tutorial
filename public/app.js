3bfe8ad14c3f9590e5c7436abda9ac3722b77726
Shaun <shaun@thenetninja.co.uk>
2020-03-16 18:52:05 +0000

    lesson-11

---
+++ public/app.js
@@ -0,0 +1,18 @@
+"use strict";
+var anchor = document.querySelector('a');
+if (anchor) {
+    console.log(anchor.href);
+}
+console.log(anchor.href);
+//const form = document.querySelector('form')!;
+var form = document.querySelector('.new-item-form');
+console.log(form.children);
+// inputs
+var type = document.querySelector('#type');
+var tofrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
---
+++ src/app.ts
@@ -0,0 +1,26 @@
+const anchor = document.querySelector('a')!;
+if(anchor) {
+  console.log(anchor.href);
+}
+console.log(anchor.href);
+
+//const form = document.querySelector('form')!;
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
+
+// inputs
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+
+  console.log(
+    type.value, 
+    tofrom.value, 
+    details.value, 
+    amount.valueAsNumber
+  );
+});
