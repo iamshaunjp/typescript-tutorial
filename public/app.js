e23b4f6acf8a8574e5ba1d1eadbf09842492a6b8
Shaun <shaun@thenetninja.co.uk>
2020-03-17 12:47:56 +0000

    lesson-15

--- public/app.js
+++ public/app.js
@@ -1,14 +1,21 @@
-import { Invoice } from './classes/Invoice.js';
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-// invOne.client = 'yoshi';
-// invOne.amount = 50;
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach(inv => {
-    console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
-});
+const me = {
+    name: 'shaun',
+    //age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent ', amount);
+        return amount;
+    },
+};
+console.log(me);
+me.speak('hello, world');
+const greetPerson = (person) => {
+    console.log('hello ', person.name);
+};
+greetPerson(me);
+//greetPerson({name: 'shaun'});
 const form = document.querySelector('.new-item-form');
 console.log(form.children);
 // inputs
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,34 @@
 import { Invoice } from './classes/Invoice.js';
 
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+// interfaces
+export interface IsPerson {
+  name: string;
+  age?: number;
+  speak(a: string): void;
+  spend(a: number): number;
+}
 
-// invOne.client = 'yoshi';
-// invOne.amount = 50;
+const me: IsPerson = {
+  name: 'shaun',
+  //age: 30,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log('I spent ', amount);
+    return amount;
+  },
+};
 
-let invoices: Invoice[] = [];
-invoices.push(invOne)
-invoices.push(invTwo);
+console.log(me);
+me.speak('hello, world');
 
-invoices.forEach(inv => {
-  console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
-})
+const greetPerson = (person: IsPerson): void => {
+  console.log('hello ', person.name);
+}
 
-
+greetPerson(me);
+//greetPerson({name: 'shaun'});
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 console.log(form.children);
