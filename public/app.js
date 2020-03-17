50b2b82906c8e0090da3f03e5ebf33f17cf3400d
Shaun <shaun@thenetninja.co.uk>
2020-03-17 14:04:33 +0000

    lesson-18

--- public/app.js
+++ public/app.js
@@ -22,3 +22,30 @@ form.addEventListener('submit', (e) => {
     }
     list.render(doc, type.value, 'end');
 });
+// GENERICS
+// const addUID = (obj: object) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+// const addUID = <T extends object>(obj: T) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: 'yoshi', age: 40 });
+//let docTwo = addUID('shaun');
+console.log(docOne.name);
+const docThree = {
+    uid: 1,
+    resourceName: 'person',
+    data: { name: 'shaun' }
+};
+const docFour = {
+    uid: 1,
+    resourceName: 'shoppingList',
+    data: ['bread', 'milk']
+};
+console.log(docThree, docFour);
--- src/app.ts
+++ src/app.ts
@@ -28,3 +28,46 @@ form.addEventListener('submit', (e: Event) => {
   
   list.render(doc, type.value, 'end');
 });
+
+// GENERICS
+
+// const addUID = (obj: object) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+
+// const addUID = <T extends object>(obj: T) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+
+const addUID = <T extends {name: string}>(obj: T) => {
+  let uid = Math.floor(Math.random() * 100);
+  return {...obj, uid};
+}
+
+let docOne = addUID({name: 'yoshi', age: 40});
+//let docTwo = addUID('shaun');
+
+console.log(docOne.name);
+
+// with interfaces
+interface Resource<T> {
+  uid: number;
+  resourceName: string;
+  data: T;
+}
+
+const docThree: Resource<object> = {
+  uid: 1, 
+  resourceName: 'person', 
+  data: { name: 'shaun' }
+};
+
+const docFour: Resource<string[]> = {
+  uid: 1, 
+  resourceName: 'shoppingList', 
+  data: ['bread', 'milk']
+};
+
+console.log(docThree, docFour);
