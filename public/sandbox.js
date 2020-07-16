51ff89a5e8ba2774c87afb6a4d4889163e20d886
Marcin Dampc <marcin.dampc@marviq.com>
2020-07-16 16:42:08 +0200

    change .ts & .js files to match the one from tutorial video

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,19 +1,7 @@
 "use strict";
-// let greet: Function = () => {
-//   console.log('hello, world');
-// }
-// greet = 'hello';
-// greet = () => {
-//   console.log('hello, again');
-// }
-var add = function (a, b, c /*?*/) {
-    if (c === void 0) { c /*?*/ = 10; }
-    console.log(a + b);
-    console.log(c);
+var logDetails = function (uid, item) {
+    console.log(item + " has a uid of " + uid);
 };
-add(5, 10, 'ninja');
-var minus = function (a, b) {
-    return a + b;
+var greet = function (user) {
+    console.log(user.name + " says hello");
 };
-var result = minus(10, 7);
-console.log(result);
