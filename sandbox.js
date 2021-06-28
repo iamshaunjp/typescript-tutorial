cf4112ee5b09143c4c0c01fbafc841d626cf86e1
Cameron Aragon <caragon4695@gmail.com>
2021-06-28 16:02:01 -0600

    Compile typescript into js

---
+++ sandbox.js
@@ -0,0 +1,10 @@
+var character = 'mario';
+var age = 30;
+var isBlackBelt = false;
+character = 'luigi';
+age = 40;
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ('hello'));
