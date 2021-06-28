1d2a8772341fc88719ac6a47b1509e71fe1568bb
Cameron Aragon <caragon4695@gmail.com>
2021-06-28 16:01:48 -0600

    Add typescript example for functions

---
+++ sandbox.ts
@@ -0,0 +1,15 @@
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
+
+character = 'luigi';
+
+age = 40;
+
+isBlackBelt = true;
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI
+}
+
+console.log(circ('hello'))
