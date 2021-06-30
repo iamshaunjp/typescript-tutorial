5164b5cc33a7e1b6e6c280dc0f3155c1b39529cb
Cameron Aragon <caragon4695@gmail.com>
2021-06-29 18:09:28 -0600

    Practice function basics in typescript

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log('test');
+var greet;
+// greet = 'hey';
+greet = function () {
+    console.log('hello, again');
+};
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, '20');
+var minus = function (a, b) {
+    return a + b;
+};
+var result = minus(10, 7);
+result = 'hello';
+console.log(result);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,23 @@
-console.log('test');
+let greet: Function;
+
+// greet = 'hey';
+
+greet = () => {
+    console.log('hello, again');
+}
+
+const add = (a: number, b: number, c: number | string = 10): void => {
+    console.log(a + b);
+    console.log(c);
+}
+
+add(5, 10, '20');
+
+const minus = (a: number, b: number): number | string => {
+    return a + b;
+}
+
+let result = minus(10, 7);
+
+result = 'hello';
+console.log(result);
