a9abb646b472305c301fb4bf4d32da6ccdccf2ba
Cameron Aragon <caragon4695@gmail.com>
2021-06-29 18:46:37 -0600

    Practice function signatures

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,4 +1,26 @@
 "use strict";
-var logDetails = function (uid, item) {
-    console.log(item + " has a uid of " + uid);
+// let greet: Function;
+var greet;
+greet = function (name, greeting) {
+    console.log(name + " says " + greeting);
 };
+var calc;
+calc = function (numOne, numTwo, action) {
+    if (action === 'add') {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
+};
+var result = calc(5, 7, 'no');
+console.log(result);
+var logDetails;
+logDetails = function (ninja) {
+    console.log(ninja.name + " is " + ninja.age + " years old");
+};
+var ninja = {
+    name: 'Sean',
+    age: 19
+};
+logDetails(ninja);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,6 +1,35 @@
-type StringOrNum = string | number;
-type objWithName = { name: string, uid: StringOrNum}
+// let greet: Function;
 
-const logDetails = (uid: StringOrNum, item: string) => {
-    console.log(`${item} has a uid of ${uid}`);
+let greet: (a: string, b: string) => void;
+
+greet = (name: string, greeting: string) => {
+    console.log(`${name} says ${greeting}`);
 }
+
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+    if (action === 'add') {
+        return numOne + numTwo;
+    } else {
+        return numOne - numTwo;
+    }
+}
+
+let result = calc(5, 7, 'no');
+console.log(result)
+
+let logDetails: (obj: {name: string, age: number}) => void;
+
+type person = {name: string, age: number};
+
+logDetails = (ninja: person) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`)
+}
+
+let ninja = {
+    name: 'Sean',
+    age: 19
+}
+
+logDetails(ninja);
