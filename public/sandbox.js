7739f5efe3ae5fe212fbeab7c6132d16106b7de5
Cameron Aragon <caragon4695@gmail.com>
2021-06-29 18:17:15 -0600

    Utilize type aliases

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,18 +1,4 @@
 "use strict";
-var greet;
-// greet = 'hey';
-greet = function () {
-    console.log('hello, again');
+var logDetails = function (uid, item) {
+    console.log(item + " has a uid of " + uid);
 };
-var add = function (a, b, c) {
-    if (c === void 0) { c = 10; }
-    console.log(a + b);
-    console.log(c);
-};
-add(5, 10, '20');
-var minus = function (a, b) {
-    return a + b;
-};
-var result = minus(10, 7);
-result = 'hello';
-console.log(result);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,23 +1,6 @@
-let greet: Function;
+type StringOrNum = string | number;
+type objWithName = { name: string, uid: StringOrNum}
 
-// greet = 'hey';
-
-greet = () => {
-    console.log('hello, again');
+const logDetails = (uid: StringOrNum, item: string) => {
+    console.log(`${item} has a uid of ${uid}`);
 }
-
-const add = (a: number, b: number, c: number | string = 10): void => {
-    console.log(a + b);
-    console.log(c);
-}
-
-add(5, 10, '20');
-
-const minus = (a: number, b: number): number | string => {
-    return a + b;
-}
-
-let result = minus(10, 7);
-
-result = 'hello';
-console.log(result);
